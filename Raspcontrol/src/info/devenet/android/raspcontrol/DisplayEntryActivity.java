50279de8fdced8d1045a7605220e066d73582741
Nicolas Devenet <github@devenet.info>
2014-02-18 22:13:27 +0100

    Load data from request JSON page into textView

--- Raspcontrol/src/info/devenet/android/raspcontrol/DisplayEntryActivity.java
+++ Raspcontrol/src/info/devenet/android/raspcontrol/DisplayEntryActivity.java
@@ -1,26 +1,54 @@
 package info.devenet.android.raspcontrol;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.StatusLine;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import info.devenet.android.raspcontrol.database.RaspDataBaseHelper;
 import info.devenet.android.raspcontrol.database.RaspcontrolContract;
-import info.devenet.android.raspcontrol.helper.HttpGetter;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.text.Layout;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.RelativeLayout;
+import android.widget.TableLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 import android.support.v4.app.NavUtils;
 
 public class DisplayEntryActivity extends Activity {
-
+	
+	Context context;
+	TableLayout layout;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_display_entry);
 		// Show the Up button in the action bar.
 		setupActionBar();
+		
+		this.context = getApplicationContext();
+		this.layout = (TableLayout) findViewById(R.id.TableLayoutEntry);
 		
 		Intent intent = getIntent();
 		long itemID = intent.getLongExtra(HomeActivity.EXTRA_ENTRY_ID, 0);
@@ -69,10 +97,11 @@ public class DisplayEntryActivity extends Activity {
 					.getString(c
 							.getColumnIndexOrThrow(RaspcontrolContract.RaspEntry.COLUMN_NAME_HOSTNAME));
 			
-			Log.d("DEBUG", itemID + ": " + itemName + " - " + itemHostname);
+			setTitle(itemName);
 			
-			HttpGetter get = new HttpGetter();
-			Log.d("DEBUG", get.execute("http://ensisa.devenet.info/server/api.php?username=fake&token=d985dfa98e44dda4cbd979affce1cf53&data=all").toString());
+					
+			HttpGetterRaspcontrol get = new HttpGetterRaspcontrol();
+			get.execute("http://ensisa.devenet.info/server/api.php?username=fake&token=d985dfa98e44dda4cbd979affce1cf53&data=all");	
 			
 		}
 		
@@ -111,4 +140,75 @@ public class DisplayEntryActivity extends Activity {
 		return super.onOptionsItemSelected(item);
 	}
 
+	private class HttpGetterRaspcontrol extends AsyncTask<String, Void, String> {
+
+		@Override
+		protected String doInBackground(String... urls) {
+			// TODO Auto-generated method stub
+			StringBuilder builder = new StringBuilder();
+			HttpClient client = new DefaultHttpClient();
+			HttpGet httpGet = new HttpGet(urls[0]);
+
+			try {
+				HttpResponse response = client.execute(httpGet);
+				StatusLine statusLine = response.getStatusLine();
+				int statusCode = statusLine.getStatusCode();
+				if (statusCode == 200) {
+					HttpEntity entity = response.getEntity();
+					InputStream content = entity.getContent();
+					BufferedReader reader = new BufferedReader(
+							new InputStreamReader(content));
+					String line;
+					while ((line = reader.readLine()) != null) {
+						builder.append(line);
+					}
+				} else {
+					Log.e("Getter", "Failed to download file");
+					return null;
+				}
+			} catch (ClientProtocolException e) {
+				e.printStackTrace();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			
+			return builder.toString();
+		}
+		
+		@Override
+	    protected void onPostExecute(String result) {
+			
+			if (result == null) {
+				Toast.makeText(context, "PROBLEM, HUSTON, WE HAVE A BIG PROBLEM!", Toast.LENGTH_LONG).show();
+				return;
+			}
+			
+			
+			try {
+				JSONObject json = new JSONObject(result);
+				
+				JSONObject rbpi = json.getJSONObject("rbpi");
+				
+				TextView tv;
+				tv = (TextView) findViewById(R.id.textViewHostname);
+				tv.setText(rbpi.getString("hostname"));
+				
+				tv = (TextView) findViewById(R.id.textViewDistribution);
+				tv.setText(rbpi.getString("distribution"));
+				
+				tv = (TextView) findViewById(R.id.textViewKernel);
+				tv.setText(rbpi.getString("kernel"));
+				
+				tv = (TextView) findViewById(R.id.textViewFirmware);
+				tv.setText(rbpi.getString("firmware"));
+				
+				
+			} catch (JSONException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+	    }
+	}
+	
+	
 }
